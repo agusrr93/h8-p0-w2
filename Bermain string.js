ca12e96995c783470e65935cb75942c4d437ea32
agusrr93 <randhania93@gmail.com>
2018-07-06 18:04:15 +0700

    bermain dengan string
    week 2

---
+++ Bermain string.js
@@ -0,0 +1,60 @@
+//tugas 1
+var word='Javascript'
+var second='is'
+var third='awesome'
+var fourth='and'
+var fifth='I'
+var six='love'
+var seventh='it'
+
+console.log(word+' '+second+' '+third+' '+fourth+' '+fifth+' '+six+' '+seventh)
+
+//tugas 2
+var word2='wow Javascript is so cool'
+var exampleFirstWord2=word2[0]+word2[1]+word2[2]
+var secondWord2=word2[4]+word2[5]+word2[6]+word2[7]+word2[8]+word2[9]+word2[10]+word2[11]+word2[12]+word2[13]
+var thirdWord2=word2[15]+word2[16]
+var fourthWord2=word2[18]+word2[19]
+var fifthWord2=word2[21]+word2[22]+word2[23]+word2[24]
+
+console.log('First Word: '+ exampleFirstWord2)
+console.log('Second Word: '+secondWord2)
+console.log('Third Word: '+thirdWord2)
+console.log('Fourth Word: '+fourthWord2)
+console.log('Fifth Word: '+fifthWord2)
+
+//tugas3
+var word3='wow Javascript is so cool'
+var exampleFirstWord3=word3.substring(0,3)
+var secondWord3=word3.substring(4,14)
+var thirdWord3=word3.substring(15,17)
+var fourthWord3=word3.substring(18,20)
+var fifthWord3=word3.substring(21,25)
+
+console.log('First Word: '+ exampleFirstWord3)
+console.log('Second Word: '+secondWord3)
+console.log('Third Word: '+thirdWord3)
+console.log('Fourth Word: '+fourthWord3)
+console.log('Fifth Word: '+fifthWord3)
+
+
+//tugas 4
+
+var word4='wow Javascript is so cool'
+var exampleFirstWord4=word4.substring(0,3)
+var secondWord4=word4.substring(4,14)
+var thirdWord4=word4.substring(15,17)
+var fourthWord4=word4.substring(18,20)
+var fifthWord4=word4.substring(21,25)
+
+var a=exampleFirstWord4.length
+var b=secondWord4.length
+var c=thirdWord4.length
+var d=fourthWord4.length
+var e=fifthWord4.length
+
+console.log('First Word: '+ exampleFirstWord4+' ,with length: '+a)
+console.log('Second Word: '+secondWord4+' ,with length: '+b)
+console.log('Third Word: '+thirdWord4+' ,with length: '+c)
+console.log('Fourth Word: '+fourthWord4+' ,with length: '+d)
+console.log('Fifth Word: '+fifthWord4+' ,with length: '+e)
