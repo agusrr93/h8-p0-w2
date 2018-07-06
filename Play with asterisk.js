0fdbba4cc9a7943d4d10c8887e44d90a0f866857
agusrr93 <randhania93@gmail.com>
2018-07-06 18:05:36 +0700

    play with asterisk
    week 2

---
+++ Play with asterisk.js
@@ -0,0 +1,36 @@
+//tugas 1
+var rows1=5
+
+for(var i=0;i<rows1;i++)
+{
+  console.log('*')
+}
+
+console.log('\n\n')
+
+//soal 2
+var rows2=5
+
+for(var i=0;i<rows2;i++)
+{
+  var h=''
+  for(var j=0;j<rows2;j++)
+  {
+    h=h+'*'
+  }
+  console.log(h+'\n')
+}
+
+console.log('\n')
+//soal 3
+var rows3=5
+
+for(var i=0;i<rows2;i++)
+{
+   var h=''
+   for(var j=0;j<i+1;j++)
+   {
+     h=h+'*'
+   }
+   console.log(h+'\n')
+}
